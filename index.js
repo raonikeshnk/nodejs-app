02491df48465d9aca3a09e58e861a3ca7af0ebbb
raonikeshnk <raonky222@gmail.com>
2024-04-20 15:53:23 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-//index.js
 
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import db from "./mongoC.js";
+
+const port = 4000;
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const apiRouter = require('./routers/api');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,28 +14,34 @@ app.use((_req, res, next) => {
     next();
   });
 
-mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
-
-const store = new MongoDBStore({
-    uri: `${process.env.DB_URL}/${process.env.DB_NAME}`,
-    collection: 'sessions'
-});
-
-app.use(session({
-    secret: process.env.KEY,
-    resave: false,
-    saveUninitialized: false,
-    store: store // Use MongoDB as session store
-}));
-
-app.use(express.static('upload'));
-app.use('/upload', express.static('upload'));
-app.use('/api', apiRouter);
+// Parses the text as url encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+// Parses the text as json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 })
 
-const PORT = process.env.PORT || 5000;
+app.post('/addUser',async (req, res) => {
+    let collection = await db.collection("users");
+    let newDocument = req.body;
+    newDocument.date = new Date();
+    let result = await collection.insertOne(newDocument);
+    console.log("rreq"+req.body);
+    res.send(result).status(204);
+});
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });
+app.get('/getUsers', async(req, res) => {
+    let collection = await db.collection("users");
+    let results = await collection.find({})
+      
+      .toArray();
+    res.send(results).status(200);
+});
+
+app.listen(port, function () {
+    console.log("Server is listening at port:" + port);
+});
+ 
